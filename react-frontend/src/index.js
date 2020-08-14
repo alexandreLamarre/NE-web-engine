dbece6dfd5df42e3d569afe8a396e3508167a03b
alexandreLamarre <alex7285@gmail.com>
2020-08-14 16:14:40 -0400

    Addded MathJax Latex renderer wrapper to webapp

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Latex from "./Latex"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Latex>
     <App />
-  </React.StrictMode>,
+  </Latex>,
   document.getElementById('root')
 );
 
