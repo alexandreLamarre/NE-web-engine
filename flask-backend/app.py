be1c507d0c83d3693710c96bfd3f6864ce97dcf6
alexandreLamarre <alex7285@gmail.com>
2020-08-19 14:38:48 -0400

    refactored routes into POST and correctly fetched and returned data

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -2,22 +2,37 @@ import flask
 import os
 from src.CommandManager import CommandManager
 
+CACHE = {}
 app = flask.Flask("__main__")
 
 @app.route("/")
 def my_index():
     return "Hello World"
 
-@app.route("/input/<commands>")
-def interpret_command(commands):
+@app.route("/input", methods = ["POST"])
+def interpret_command():
 
-    new_command_manager = CommandManager(commands)
+    req = flask.request.get_json()
+
+    new_command_manager = CommandManager(req)
     #
     label , sublabels_and_frame_info_tuple = new_command_manager.run_initial()
-    # resp = {"input": [label, sublabels_and_frame_info_tuple] }
-    # res = flask.make_response(flask.jsonify({"message":"Ok"}),200)
-    return flask.jsonify({"label": label, "interpreted": sublabels_and_frame_info_tuple[0],\
-                                    "uninterpreted": sublabels_and_frame_info_tuple[1],\
-                                    "Errors": sublabels_and_frame_info_tuple[2]})
+
+    return flask.jsonify({"label": label, "interpreted": sublabels_and_frame_info_tuple[0][1],\
+                                    "uninterpreted": sublabels_and_frame_info_tuple[1][1],\
+                                    "Errors": sublabels_and_frame_info_tuple[2][1]})
+
+@app.route("/input/commands", methods = ["POST"])
+def run_commands():
+
+    req = flask.request.get_json()
+
+    new_command_manager = CommandManager(req)
+    #
+    label , sublabels_and_frame_info_tuple = new_command_manager.run_next()
+    while(label, sublabels_and_frame_info_tuple) != (None,None):
+        label , sublabels_and_frame_info_tuple = new_command_manager.run_next()
+
+    return flask.jsonify({"commmands": None})
 
 app.run(debug = True)
