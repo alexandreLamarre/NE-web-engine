4a228c941a612731e0ebdb6cff6621fb5a17ef38
alexandreLamarre <alex7285@gmail.com>
2020-08-18 22:05:29 -0400

    returns the initial command of interpreting the input

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -8,12 +8,16 @@ app = flask.Flask("__main__")
 def my_index():
     return "Hello World"
 
-@app.route("/input/<string:commands>")
+@app.route("/input/<commands>")
 def interpret_command(commands):
-    new_command_manager = CommandManager(commands)
 
+    new_command_manager = CommandManager(commands)
+    #
     label , sublabels_and_frame_info_tuple = new_command_manager.run_initial()
-    resp = {"input": [label, sublabels_and_frame_info_tuple] }
-    return flask.jsonify(resp)
+    # resp = {"input": [label, sublabels_and_frame_info_tuple] }
+    # res = flask.make_response(flask.jsonify({"message":"Ok"}),200)
+    return flask.jsonify({"label": label, "interpreted": sublabels_and_frame_info_tuple[0],\
+                                    "uninterpreted": sublabels_and_frame_info_tuple[1],\
+                                    "Errors": sublabels_and_frame_info_tuple[2]})
 
 app.run(debug = True)
