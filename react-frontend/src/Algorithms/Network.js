b22cc8ca51903b76c68ff9dcb3ee1692127b87c6
alexandreLamarre <alex7285@gmail.com>
2020-09-08 16:14:33 -0400

    Finally animated the Network

--- react-frontend/src/Algorithms/Network.js
+++ react-frontend/src/Algorithms/Network.js
@@ -3,6 +3,49 @@ import {forceDirectedLayout} from "./forceDirectedAlgo";
 
 import "./Network.css";
 
+// function animateNetwork(animations, canvas, og_vertices, og_edges, delta, width,height){
+//   const ctx = canvas.getContext("2d");
+//   let vertices = [];
+//   let edges = [];
+//
+//   for(let i= 0; i < og_vertices.length; i++){
+//     vertices.push(og_vertices[i].slice());
+//   }
+//
+//   for(let i = 0; i < og_vertices.length; i++){
+//     for(let j = 0; j < og_vertices.length; j++){
+//       for(let k =0; k < og_edges.length; k++){
+//         if(og_vertices[i] === og_edges[k][0] && og_vertices[j] === og_edges[k][1]){
+//           edges.push(vertices[i],vertices[j]);
+//         }
+//       }
+//     }
+//   }
+//
+//   for(let k = 0; k < animations.length; k++){
+//     for(let i = 0; i < vertices.length; i++){
+//       vertices[i] = animations[k][i];
+//     }
+//     setTimeout(() => {
+//       // ctx.clearRect(0,0,width,height);
+//       for(let i =0; i < vertices.length; i++){
+//         ctx.fillStyle= "#FF0000"
+//         ctx.fillRect(vertices[i][0],vertices[i][1], 6, 6);
+//       }
+//
+//       for(let j = 0; j < edges.length; j++){
+//         ctx.beginPath();
+//         ctx.globalAlpha = 0.2;
+//         ctx.moveTo(edges[j][0][0]+3, edges[j][0][1]+3);
+//         ctx.lineTo(edges[j][1][0]+3, edges[j][1][1]+3);
+//         ctx.stroke();
+//         ctx.closePath();
+//       }
+//     }, k *50);
+//   }
+// }
+
+
 class NetworkVisualizer extends React.Component{
   constructor(props){
     super(props);
@@ -41,8 +84,12 @@ class NetworkVisualizer extends React.Component{
     for(let j = 0; j < this.state.edges.length; j++){
       ctx.beginPath();
       ctx.globalAlpha = 0.2;
-      ctx.moveTo(this.state.edges[j][0][0]+3, this.state.edges[j][0][1]+3);
-      ctx.lineTo(this.state.edges[j][1][0]+3, this.state.edges[j][1][1]+3);
+      const index1 = this.state.edges[j][0];
+      const index2 = this.state.edges[j][1];
+      ctx.moveTo(this.state.vertices[index1][0],this.state.vertices[index1][1] );
+      ctx.lineTo(this.state.vertices[index2][0],this.state.vertices[index2][1] );
+      // ctx.moveTo(this.state.vertices[j][0][0]+3, this.state.edges[j][0][1]+3);
+      // ctx.lineTo(this.state.edges[j][1][0]+3, this.state.edges[j][1][1]+3);
       ctx.stroke();
       ctx.closePath();
     }
@@ -52,26 +99,22 @@ class NetworkVisualizer extends React.Component{
     const values = forceDirectedLayout(this.state.vertices, this.state.edges,this.state.width, this.state.height);
     const new_vertices = values[0];
     const animations = values[1];
-    // console.log(new_vertices);
+    console.log(animations);
     // console.log(animations);
-    this.setState({vertices: new_vertices});
-    // this.animateNetwork(animations);
+    // // animateNetwork(animations, this.canvas.current,this.state.vertices, this.state.edges, this.state.delta, this.state.width,this.state.height);
+    this.animateNetwork(animations);
   }
-  // animateNetwork(animations){
-  //   for(let i = 0; i < animations.length; i++){
-  //     setTimeout(() => {
-  //       const vertices = this.stateRef.current.vertices;
-  //       for(let j = 0; j < vertices.length; j++){
-  //         vertices[j][0] += this.delta* animations[i][j][0];
-  //         vertices[j][1] += this.delta* animations[i][j][1];
-  //       }
-  //       this.setState({vertices: vertices})
-  //     }, i * 50);
-  //   }
-  // }
+
+  animateNetwork(animations){
+    for(let k = 0; k < animations.length; k++){
+      setTimeout(() => {
+        this.setState({vertices: animations[k]});
+      }, k*50)
+    }
+  }
 
   resetNetwork(){
-    const [vertices, edges] = createRandomNetwork(this.state.width, this.state.height, this.state.numV, this.state.numE);
+    const [vertices, edges] = createRandomNetwork(this.state.width, this.state.height, this.state.numV, this.state.numE, this.state.width, this.state.height);
 
     this.setState({vertices: vertices, edges: edges})
   }
@@ -132,5 +175,5 @@ function createRandomPos(maxWidth, maxHeight){
 function connectRandomVertices(end, vertices){
   var random1 = Math.floor(Math.random()*end);
   var random2 = Math.floor(Math.random()*end);
-  return [vertices[random1], vertices[random2]];
+  return [random1, random2];
 }
--- react-frontend/src/Algorithms/forceDirectedAlgo.js
+++ react-frontend/src/Algorithms/forceDirectedAlgo.js
@@ -1,5 +1,5 @@
-const CREP = 30;
-const CSPRING = 10;
+const CREP = 20;
+const CSPRING = 20;
 var lx = 0;
 var ly = 0;
 /**
@@ -9,19 +9,22 @@ export function forceDirectedLayout(vertices,edges,graph_distancex, graph_distan
   // relevant constants for spring embedding
   lx = graph_distancex;
   ly = graph_distancey;
-  const K = iterations === undefined ? 300: iterations;
-  const epsilon = threshold === undefined? 0.01: threshold;
-  const delta = constant === undefined? 2: constant;
+  const K = iterations === undefined ? 100: iterations;
+  const epsilon = threshold === undefined? 0.1: threshold;
+  const delta = constant === undefined? 1.5: constant;
 
   //make copies of input
-  let new_vertices = vertices.slice();
+  let new_vertices = [];
+  for(let i= 0; i < vertices.length; i++){
+    new_vertices.push(vertices[i].slice());
+  }
   let new_edges = [];
   //make a copy of edges and copy references of new_vertices
-  for(let i = 0; i < new_vertices.length; i++){
-    for(let j = 0; j < new_vertices.length; j++){
+  for(let i = 0; i < vertices.length; i++){
+    for(let j = 0; j < vertices.length; j++){
       for(let k =0; k < edges.length; k++){
-        if(vertices[i] === edges[k][0] && vertices[j] === edges[k][1]){
-          new_edges.push(new_vertices[i],new_vertices[j]);
+        if(i === edges[k][0] && j === edges[k][1]){
+          new_edges.push(new_vertices[i].slice(),new_vertices[j].slice());
         }
       }
     }
@@ -38,16 +41,16 @@ export function forceDirectedLayout(vertices,edges,graph_distancex, graph_distan
       let vert_connected = []; //represents vertices we should not repulse later
       for(let j = 0; j < new_edges.length; j++){
         //vertices should attract
-        if(new_vertices[i] === new_edges[j][0] && new_vertices[i] !== new_edges[j][1]){
-          const calcs = fattract(new_edges[j][0], new_edges[j][1]);
+        if(i === new_edges[j][0] && i !== new_edges[j][1]){
+          const calcs = fattract(new_vertices[new_edges[j][0]], new_vertices[new_edges[j][1]]);
           // console.log(calcs);
           f[0] += calcs[0]; // should be two dimensional
           f[1] += calcs[1];
           vert_connected.push(new_edges[j][1]);
         }
         //vertices should attract
-        if(new_vertices[i] === new_edges[j][1] && new_vertices[i] !== new_edges[j][0]){
-          const calcs = fattract(new_edges[j][0], new_edges[j][1])
+        if(i === new_edges[j][1] && i !== new_edges[j][0]){
+          const calcs = fattract(new_vertices[new_edges[j][0]], new_vertices[new_edges[j][1]])
           f[0] += calcs[0]; // should be two dimensional
           f[1] += calcs[1];
           vert_connected.push(new_edges[j][0]);
@@ -57,7 +60,7 @@ export function forceDirectedLayout(vertices,edges,graph_distancex, graph_distan
         if(i === j ) continue;
         let connected = false;
         for(let k = 0; k < vert_connected.length; k++){
-          if(j === k) connected = true;
+          if(j === vert_connected[k]) connected = true;
         }
         // vertices should repluse one another
         if(!connected){
@@ -68,24 +71,34 @@ export function forceDirectedLayout(vertices,edges,graph_distancex, graph_distan
       }
       force_list.push(f)
     }
-    animations.push(force_list);
+    const iteration_animation = [];
     for(let i = 0; i < new_vertices.length; i++){
       const new_x = new_vertices[i][0] + delta*force_list[i][0];
       const new_y = new_vertices[i][1] + delta*force_list[i][1];
       new_vertices[i][0] = new_x//(new_x > graph_distancex-3)? (graph_distancex-3): ((new_x-3) < 0)? 0: (new_x-3); // should be two dimensional
       new_vertices[i][1] = new_y//(new_y > graph_distancey-3)? (graph_distancey-3): ((new_y -3)< 0)? 0: (new_y-3);
+      iteration_animation.push(new_vertices[i].slice());
       const max_dist = distance(force_list[i], [0,0]);
-      maxFvt = maxFvt > max_dist? maxFvt: max_dist;
+      if(maxFvt === Infinity){
+        maxFvt = max_dist;
+      }
+      else{
+        maxFvt = maxFvt > max_dist? maxFvt: max_dist;
+      }
     }
+    animations.push(iteration_animation);
     t += 1
   }
   // animations.push(new_vertices.slice());
+  const iteration_animation = [];
   for(let i = 0; i < new_vertices.length; i ++){
     if(new_vertices[i][0] < 0) new_vertices[i][0] = 0;
     if(new_vertices[i][1] < 0)new_vertices[i][1] = 0;
     if(new_vertices[i][0] > graph_distancex -6) new_vertices[i][0] = graph_distancex-6;
     if(new_vertices[i][1] > graph_distancey - 6) new_vertices[i][1] = graph_distancey-6;
+    iteration_animation.push(new_vertices[i].slice())
   }
+  animations.push(iteration_animation);
   return [new_vertices, animations];
 }
 
